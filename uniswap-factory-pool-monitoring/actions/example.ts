f24fa21a8170b95dca764e26a180e82deb67797c
dzekicb <bogdan.dzekic@tenderly.co>
2024-05-03 15:19:59 +0200

    additional changes

--- uniswap-factory-pool-monitoring/actions/example.ts
+++ uniswap-factory-pool-monitoring/actions/example.ts
@@ -1,46 +1,43 @@
+import { Network as ActionsNetwork, Context, AlertEvent } from '@tenderly/actions';
+import { Network as SDKNetwork, Tenderly } from '@tenderly/sdk';
 const ethers = require('ethers');
-import { Network, Context, AlertEvent } from '@tenderly/actions';
-const axios = require('axios');
 
 // Do not change function name.
 const actionFn = async (context: Context, alertEvent: AlertEvent) => {
 
-	const provider = new ethers.providers.JsonRpcProvider(context.gateways.getGateway(Network.MAINNET));
+	const provider = new ethers.providers.JsonRpcProvider(context.gateways.getGateway(ActionsNetwork.MAINNET));
 	const key = await context.secrets.get('ACCESS-KEY')
-	const networkId = "1";
-	const tagName = "pool";
+
+	const accountSlug = '';
+	const projectSlug = '';
+	const tagName = 'pool';
+
+	const tenderly = new Tenderly({
+		accountName: accountSlug,
+		projectName: projectSlug,
+		accessKey: key,
+		network: SDKNetwork.MAINNET, // Replace with the appropriate network
+	});
 
 	const resp = await provider.send("tenderly_traceTransaction", [`${alertEvent.hash}`]);
 	const poolCreatedLogs = resp.logs.filter((log: { name: string; }) => log.name === 'PoolCreated');
 	const poolInput = poolCreatedLogs[0].inputs.find((input: { name: string; }) => input.name === 'pool');
 	const poolAddress = poolInput.value;
 
-	async function callYourApi(childContractAddress: any) {
-		const axiosInstance = axios.create({
-			baseURL: 'https://api.tenderly.co/api/v1'
-		});
-		axiosInstance.defaults.headers.common['X-Access-Key'] = key;
-
-		const contractData = {
-			"network_id": `${networkId}`,
-			"address": `${childContractAddress}`
-		};
-
-		const tagData = {
-			"tag":`${tagName}`,
-			"contract_ids":[`eth:${networkId}:${childContractAddress}`]
-		}
-
+	async function addContract(childContractAddress: string) {
 		try {
-			await axiosInstance.post(`account/me/project/monitoring/address`, contractData);
-			await axiosInstance.post(`account/me/project/monitoring/tag`, tagData);
+			await tenderly.contracts.add(childContractAddress, {
+				displayName: 'Pool'
+			});
+			await tenderly.contracts.update(childContractAddress, { appendTags: [tagName] });
 			console.log(`Pool contract is: ${childContractAddress}, and has been added with tag ${tagName}`);
-		} catch (error) {
-			console.error("Error calling Tenderly API:", error);
+		}
+		catch (error) {
+			console.error('Error adding contract:', error);
 		}
 	}
 
-	await callYourApi(poolAddress);
+	await addContract(poolAddress);
 };
 
 // Do not change this.
